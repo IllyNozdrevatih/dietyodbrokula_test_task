385722ce03e8ef607ce4faeb5c19b677609a9b4f
IllaNozdrevatih <i.nozdrevatykh@upswot.com>
2023-04-19 11:58:07 +0200

    add layout for products

---
+++ src/mockups/products.mockup.js
@@ -0,0 +1,76 @@
+export const productsMockup = [
+  {
+    name: "Chicken Alfredo",
+    cost: 12.99,
+    image_url: "https://bellyfull.net/wp-content/uploads/2021/02/Chicken-Alfredo-blog-4-768x1024.jpg",
+    description:
+      "A classic Italian dish made with fettuccine pasta and tender chicken in a rich and creamy Alfredo sauce. Perfect for a cozy night in or a special occasion.",
+    rating: 4.5,
+  },
+  {
+    name: "Beef Stroganoff",
+    cost: 14.99,
+    image_url: "https://www.gimmesomeoven.com/wp-content/uploads/2020/10/Beef-Stroganoff-Recipe-9.jpg",
+    description:
+      "A hearty and comforting dish that features tender beef and mushrooms in a creamy sour cream sauce. Serve it over egg noodles for a satisfying meal.",
+    rating: 4.3,
+  },
+  {
+    name: "Shrimp Scampi",
+    cost: 16.99,
+    image_url:
+      "https://www.allrecipes.com/thmb/jiV_4f8vXFle1RdFLgd8-_31J3M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/229960-shrimp-scampi-with-pasta-DDMFS-4x3-e065ddef4e6d44479d37b4523808cc23.jpg",
+    description:
+      "A quick and easy seafood dish that's bursting with garlic and lemon flavors. Serve it over pasta or with a side of crusty bread to soak up the delicious sauce.",
+    rating: 4.8,
+  },
+  {
+    name: "Spicy Tofu Stir Fry",
+    cost: 12.99,
+    image_url:
+      "https://www.simplyrecipes.com/thmb/eSjn6vsdKpQIdrD4msJcO41Wm84=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Spicy-Tofu-Stirfry-LEAD-3-3da17681605c41688be8742e045f71f5.jpg",
+    description:
+      "A healthy and flavorful vegetarian dish that's loaded with protein and vegetables. The spicy sauce is made with garlic, ginger, soy sauce, and chili paste.",
+    rating: 3.9,
+  },
+  {
+    name: "Lobster Bisque",
+    cost: 18.99,
+    image_url: "https://cafedelites.com/wp-content/uploads/2020/02/Lobster-Bisque-IMAGE-2.jpg",
+    description:
+      "A rich and creamy soup that's made with tender chunks of lobster, cream, and spices. Serve it with a side of crusty bread or crackers for a satisfying meal.",
+    rating: 4.7,
+  },
+  {
+    name: "Beef and Vegetable Kabobs",
+    cost: 15.99,
+    image_url: "https://suebeehomemaker.com/wp-content/uploads/2020/04/grilled-steak-kabobs-with-vegetables.jpg",
+    description:
+      "A colorful and flavorful dish that's perfect for summer grilling. The beef and vegetables are marinated in a tangy and savory sauce for maximum flavor.",
+    rating: 4.1,
+  },
+  {
+    name: "Vegetable Curry",
+    cost: 13.99,
+    image_url: "https://www.recipetineats.com/wp-content/uploads/2017/07/Vegetable-Curry.jpg",
+    description:
+      "A spicy and aromatic dish that's made with a variety of vegetables and spices. Serve it over rice for a filling and satisfying meal.",
+    rating: 3.7,
+  },
+  {
+    name: "Pan-Seared Salmon",
+    cost: 17.99,
+    image_url: "https://cafedelites.com/wp-content/uploads/2017/03/Best-Crispy-Lemon-Garlic-Herb-Salmon-IMAGES-19.jpg",
+    description:
+      "A healthy and flavorful meal that's quick and easy to make. The salmon is seasoned with herbs and spices, and cooked to crispy perfection.",
+    rating: 4.6,
+  },
+  {
+    name: "Roasted Vegetable Lasagna",
+    cost: 14.99,
+    image_url: "https://familystylefood.com/wp-content/uploads/2017/09/roasted-vegetable-lasagna-familystylefood.jpg",
+    description:
+      "A healthy and flavorful meal that's quick and easy to make. The salmon is seasoned with herbs and spices, and cooked to crispy perfection.",
+    rating: 4.6,
+  },
+]
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,85 @@
 <template>
   <div class="container">
     <h1>Home page</h1>
+
+    <v-row v-if="productsMockup.length">
+      <v-col lg="4"
+           v-for="(productItem, index) of productsMockup"
+          :key="index"
+      >
+        <v-card
+            :loading="loading"
+            class="mx-auto my-12"
+            max-width="374"
+        >
+          <template v-slot:loader="{ isActive }">
+            <v-progress-linear
+                :active="isActive"
+                color="deep-purple"
+                height="4"
+                indeterminate
+            ></v-progress-linear>
+          </template>
+
+          <v-img
+              cover
+              height="250"
+              :src="productItem.image_url"
+          />
+
+          <v-card-title v-text="productItem.name"/>
+
+          <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0"
+            >
+              <v-rating
+                  :value="productItem.rating"
+                  color="amber"
+                  dense
+                  half-increments
+                  readonly
+                  size="14"
+              ></v-rating>
+            </v-row>
+            <div class="my-4 text-subtitle-1">
+              zł • {{ productItem.cost }}
+            </div>
+          </v-card-text>
+          <v-card-subtitle>
+            <span class="me-1" v-text="productItem.description"/>
+
+            <v-icon
+                color="error"
+                icon="mdi-fire-circle"
+                size="small"
+            ></v-icon>
+          </v-card-subtitle>
+
+          <v-divider class="mx-4 mb-1"></v-divider>
+
+        </v-card>
+      </v-col>
+    </v-row>
+
   </div>
 </template>
 
 <script>
+import {productsMockup} from "@/mockups/products.mockup";
 
-  export default {
-    name: 'Home',
 
-    components: {
-    },
-  }
+export default {
+  name: 'Home',
+  data(){
+    return {
+      productsMockup: productsMockup,
+      loading: false,
+      selection: 1,
+    }
+  },
+  components: {
+  },
+}
 </script>
